c7b37c2cb47473d6a65418cd03bc65346ac92c92
mafterr11 <mafterr11@gmail.com>
2024-02-19 22:41:50 +0200

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,10 +10,10 @@ const roboto = Roboto({
 });
 
 export const metadata = {
-  title: "Maftei - Web Developer",
+  title: "Myriad - Web Development",
   description:
     "Hello, my name is Maftei Alexandru. Lets start this journey together and create your dream website! ",
-  keywords: "web development, frontend, web design, freelancer, bucuresti, romania, small buisiness, maftei alexandru, site prices"
+  keywords: "web development, frontend, web design, freelancer, bucuresti, romania, small buisiness, maftei alexandru, site prices, myriad tech"
 };
 
 export default function RootLayout({ children }) {
