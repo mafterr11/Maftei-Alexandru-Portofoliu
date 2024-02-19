0ad1e1e7b06d4c52d6f97b508a0cbbaf160e4bfd
mafterr11 <mafterr11@gmail.com>
2024-02-19 21:48:52 +0200

    .

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
     const formData = await request.json();
 
     const data = await resend.emails.send({
-      from: "office@myriad.ro",
+      from: "office@myriad-tech.ro",
       to: ["alexandrumaftei95@gmail.com"],
       subject: "MAF Services - Solicitare Nouă",
       react: EmailTemplate(formData), // pass the entire formData object to the template
