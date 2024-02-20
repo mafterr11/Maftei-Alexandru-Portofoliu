606dfb393dde77d4a257ef969f4501cecda83495
mafterr11 <mafterr11@gmail.com>
2024-02-21 00:39:15 +0200

    .

--- components/ui/switch.jsx
+++ components/ui/switch.jsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 const Switch = React.forwardRef(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-[3.2rem] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent/30 data-[state=unchecked]:bg-gray-200 dark:focus-visible:ring-gray-300 dark:focus-visible:ring-offset-black dark:data-[state=checked]:bg-gray-900 dark:data-[state=unchecked]:bg-gray-800",
+      "peer inline-flex h-6 w-[3.2rem] shrink-0 cursor-pointer items-center rounded-full border border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent/30 data-[state=unchecked]:bg-gray-200 dark:focus-visible:ring-gray-300 dark:focus-visible:ring-offset-black dark:data-[state=checked]:bg-gray-900 dark:data-[state=unchecked]:bg-gray-800",
       className
     )}
     {...props}
