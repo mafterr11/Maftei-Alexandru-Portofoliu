a5e8c34c44bd5eac88b2fa2771e130f9bd0ba317
mafterr11 <mafterr11@gmail.com>
2024-02-21 00:38:01 +0200

    .

--- components/LocalSwitcher.jsx
+++ components/LocalSwitcher.jsx
@@ -1,40 +1,36 @@
 import { useLocale } from "next-intl";
-import { useRouter } from "next/navigation"; // Correcting the import path
+import { usePathname, useRouter } from "next/navigation";
 import { useTransition } from "react";
-import { Switch } from "@/components/ui/switch";
+import { Switch } from "@/components/ui/switch"; // Ensure this is the correct import path
 import { Label } from "./ui/label";
 
 export default function LocalSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localeActive = useLocale();
+  const pathname = usePathname();
+  const actualPath = `/${pathname.split('/').filter(Boolean).slice(1).join('/')}`;
 
   const toggleLocale = () => {
     const nextLocale = localeActive === "en" ? "ro" : "en";
     startTransition(() => {
-      router.replace(`/${nextLocale}`);
+      router.replace(`/${nextLocale}${actualPath}`);
     });
   };
 
+  // Adjusted to reflect a proper switch behavior
   return (
     <div className="flex items-center justify-center relative">
       <Switch
-        as="button"
+        checked={localeActive === "en"} // Assuming "en" is the "checked" state
+        onCheckedChange={toggleLocale} // This prop handles change events
         disabled={isPending}
-        onClick={toggleLocale}
-        aria-checked={localeActive === "en"}
-        role="switch"
         id="language-switch"
+        aria-label="Language Switch" // Providing an accessible name
       />
-      {localeActive === "en" ? (
-        <Label htmlFor="language-switch" className="absolute top-[0.35rem] right-[0.50rem]">
-          EN
-        </Label>
-      ) : (
-        <Label htmlFor="language-switch" className="absolute top-[0.35rem] right-[0.50rem]">
-          RO
-        </Label>
-      )}
+      <Label htmlFor="language-switch" className={`absolute top-[0.35rem] ${localeActive === "en" ? "left-[0.50rem]" : "right-[0.50rem]"}`}>
+        {localeActive.toUpperCase()}
+      </Label>
     </div>
   );
 }
--- components/ui/switch.jsx
+++ components/ui/switch.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 
@@ -8,14 +6,14 @@ import { cn } from "@/lib/utils"
 const Switch = React.forwardRef(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-[3.2rem] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=unchecked]:bg-slate-200 dark:focus-visible:ring-slate-300 dark:focus-visible:ring-offset-slate-950 dark:data-[state=checked]:bg-slate-50 dark:data-[state=unchecked]:bg-slate-800",
+      "peer inline-flex h-6 w-[3.2rem] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent/30 data-[state=unchecked]:bg-gray-200 dark:focus-visible:ring-gray-300 dark:focus-visible:ring-offset-black dark:data-[state=checked]:bg-gray-900 dark:data-[state=unchecked]:bg-gray-800",
       className
     )}
     {...props}
     ref={ref}>
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-accent shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-slate-950"
+        "pointer-events-none block h-5 w-5 rounded-full bg-accent shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[1.65rem] data-[state=unchecked]:translate-x-[0.04rem] dark:bg-black"
       )} />
   </SwitchPrimitives.Root>
 ))
