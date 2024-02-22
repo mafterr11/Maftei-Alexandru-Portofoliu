9b0977b3aa2dfac3dfa418a77a7669561d3341ee
Alexandru Maftei <mafterr11@gmail.com>
2024-02-22 15:52:28 +0200

    .

--- components/LocalSwitcher.jsx
+++ components/LocalSwitcher.jsx
@@ -12,7 +12,7 @@ export default function LocalSwitcher() {
   const actualPath = `/${pathname.split('/').filter(Boolean).slice(1).join('/')}`;
 
   const toggleLocale = () => {
-    const nextLocale = localeActive === "en" ? "en" : "ro";
+    const nextLocale = localeActive === "en" ? "ro" : "en";
     startTransition(() => {
       router.replace(`/${nextLocale}${actualPath}`);
     });
