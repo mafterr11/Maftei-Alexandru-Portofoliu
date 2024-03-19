1b7665da61315ad386d49f277d03afe7077b0306
Alexandru Maftei <mafterr11@gmail.com>
2024-03-19 11:59:56 +0200

    .

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
+import { Send } from "lucide-react";
 
 const Cta = () => {
   const t = useTranslations("Cta");
+  const tLink = useTranslations("Hero");
 
   return <section className="py-24 bg-accent/10 mb-24">
     <div  className="container mx-auto">
@@ -12,9 +14,11 @@ const Cta = () => {
                 {t("title")}
                 <span>{t("title2")}</span>
             </h2>
-            <Link href="/contact">
-                <Button className="text-white">{t("contact-button")}</Button>
-            </Link>
+            <Link href={tLink('contact-link')}>
+                <Button className="gap-x-2"> 
+                {tLink('contact-button')} <Send size={18} />
+                </Button>
+              </Link>
         </div>
     </div>
   </section>;
