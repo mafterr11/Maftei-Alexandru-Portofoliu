270f267e1403e15db213b5c18e9fd0b58960d25b
Alexandru Maftei <mafterr11@gmail.com>
2024-04-24 12:21:53 +0300

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Myriad - Web Development",
+  description:
+    "Hello, my name is Maftei Alexandru. Lets start this journey together and create your dream website! ",
+  keywords:
+    "web development, frontend, web design, freelancer, bucuresti, romania, small buisiness, maftei alexandru, site prices, myriad tech",
+};
+
 export default function RootLayout({ children }) {
   return children;
 }
