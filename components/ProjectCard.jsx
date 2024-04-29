a529b9f1a6b53f0f335f96b01857320ec996bbe9
mafterr11 <mafterr11@gmail.com>
2024-04-29 22:52:17 +0300

    .

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -19,7 +19,7 @@ const ProjectCard = ({ project }) => {
             priority
           />
           {/* btns */}
-          <div className="flex flex-col gap-y-4 absolute top-4 right-4">
+          <div className="flex flex-col gap-y-4 absolute top-4 right-2">
             <Link
               href={project.link}
               className="hover:bg-accent/70 w-[45px] h-[45px] rounded-md flex justify-center items-center xl:scale-0 xl:opacity-0 max-lg:opacity-100 max-lg:scale-100 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300 bg-black/90"
