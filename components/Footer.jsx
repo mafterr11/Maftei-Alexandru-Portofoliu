8b1b8db20672a9fd7fac31a93d2577a679bdb5fa
mafterr11 <mafterr11@gmail.com>
2024-04-29 19:33:18 +0300

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 const Footer = () => {
   const t = useTranslations("Footer");
-
+  const currentYear = new Date().getFullYear(); 
   return (
     <footer className='py-4 border-t-2 border-accent bg-body-light'>
       <div className='container mx-auto'>
         <div className='flex flex-col items-center justify-center'>
           <Socials className='flex gap-x-6 text-[20px]' />
           <div className='text-center my-2'>
-            Copyright &copy; <a href="https://www.linkedin.com/in/maftei-alexandru/" className="hover "><span className="text-accent">Maftei Alexandru.</span></a> {t("copyright")}
+            Copyright &copy; <a href="https://www.linkedin.com/in/maftei-alexandru/" className="hover">{currentYear} <span className="text-accent">Maftei Alexandru.</span></a>
           </div>
           <div className='text-center font-semibold underline'>
             <Link href={t("gdpr")}>{t("name")}</Link>
