2e2dd6b19a852f51d7753ec8494a3a24eae5f647
mafterr11 <mafterr11@gmail.com>
2024-05-01 13:04:50 +0300

    .

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -7,21 +7,25 @@ const Cta = () => {
   const t = useTranslations("Cta");
   const tLink = useTranslations("Hero");
 
-  return <section className="py-24 bg-body-light mb-44">
-    <div  className="container mx-auto">
-        <div className="flex flex-col items-center">
-            <h2 className="max-w-3xl text-center mb-6 flex flex-col items-center justify-center gap-y-2">
-                {t("title")}
-                <span className="text-3xl md:max-xl:text-4xl font-medium">{t("title2")}</span>
-            </h2>
-            <Link href={tLink('contact-link')}>
-                <Button className="gap-x-2"> 
-                {tLink('contact-button')} <Send size={18} />
-                </Button>
-              </Link>
+  return (
+    <section className='py-24 bg-body-light mb-44'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col items-center'>
+          <h2 className='max-w-3xl text-center mb-6 flex flex-col items-center justify-center gap-y-2'>
+            {t("title")}
+            <span className='text-3xl md:max-xl:text-4xl font-medium'>
+              {t("title2")}
+            </span>
+          </h2>
+          <Link href={tLink("contact-link")}>
+            <Button className='gap-x-2'>
+              {tLink("contact-button")} <Send size={18} />
+            </Button>
+          </Link>
         </div>
-    </div>
-  </section>;
+      </div>
+    </section>
+  );
 };
 
 export default Cta;
