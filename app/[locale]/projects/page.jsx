2b3f5269a42987307ed054c744de5dcb692085b3
mafterr11 <mafterr11@gmail.com>
2024-05-01 15:29:20 +0300

    .

--- app/[locale]/projects/page.jsx
+++ app/[locale]/projects/page.jsx
@@ -4,7 +4,8 @@ import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 import ProjectCard from "@/components/ProjectCard";
 import { useTranslations } from "next-intl";
 import { Dot } from "@/components/Dot";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
 
 const Projects = () => {
   const t = useTranslations("Proiecte");
@@ -33,7 +34,7 @@ const Projects = () => {
       link: "/",
       github: "/",
     },
-     {
+    {
       image: "/work/monte-bianco.png",
       category: "In Progress",
       name: "Monte Bianco",
@@ -41,7 +42,6 @@ const Projects = () => {
       link: "https://montebianco.vercel.app",
       github: "/",
     },
-    
   ];
   const uniqueCategories = [
     t("page.tab"),
@@ -51,27 +51,36 @@ const Projects = () => {
   const [category, setCategory] = useState(t("page.tab"));
   const filteredProjects = projectData.filter((project) => {
     // if category  is all projects return all projects, else filter
-    return category === t("page.tab")
-      ? project
-      : project.category === category;
+    return category === t("page.tab") ? project : project.category === category;
   });
 
   return (
-    <section className="min-h-screen pt-40">
-      <div className="container mx-auto">
-        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+    <section className='min-h-screen pt-40'>
+      <div className='container mx-auto'>
+        <motion.h2
+          variants={fadeIn("down", 0.2)}
+          initial='hidden'
+          whileInView={"show"}
+          viewport={{ once: true, ammount: 0.4 }}
+          className='section-title mb-8 xl:mb-16 text-center mx-auto'
+        >
           <Dot />
           {t("page.title")}
-        </h2>
+        </motion.h2>
         {/* tabs */}
-        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
-          <TabsList className="w-full grid h-full md:grid-cols-3 lg:max-w-[640px] mb-12 mx-auto md:border">
+        <motion.div
+          variants={fadeIn("down", 0.5)}
+          initial='hidden'
+          whileInView={"show"}
+          viewport={{ once: true, ammount: 0.4 }}>
+        <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
+          <TabsList className='w-full grid h-full md:grid-cols-3 lg:max-w-[640px] mb-12 mx-auto md:border'>
             {categories.map((category, index) => {
               return (
                 <TabsTrigger
                   value={category}
                   key={index}
-                  className="capitalize w-[162px] md:w-auto"
+                  className='capitalize w-[162px] md:w-auto'
                   onClick={() => setCategory(category)}
                 >
                   {category}
@@ -80,14 +89,17 @@ const Projects = () => {
             })}
           </TabsList>
           {/* tabs content */}
-          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
-            {filteredProjects.map((project, index)=>{
-              return <TabsContent value={category} key={index}>
-                <ProjectCard project={project}/>
-              </TabsContent>
+          <div className='text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
             })}
           </div>
         </Tabs>
+        </motion.div>
       </div>
     </section>
   );
