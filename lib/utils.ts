494c17aa7f69808ec194463815ee4e5849795c16
mafterr11 <mafterr11@gmail.com>
2024-05-13 21:01:15 +0300

    .

--- lib/utils.ts
+++ lib/utils.ts
@@ -36,6 +36,6 @@ export function constructMetadata({
       creator: "@myriad-tech",
     },
     icons,
-    metadataBase: new URL("https://www.myriad-tech.ro"),
+    metadataBase: new URL("https://myriad-tech.ro"),
   };
 }
