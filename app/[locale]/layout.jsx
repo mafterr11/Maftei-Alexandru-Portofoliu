8952658d1240d568ac80f274554f44ae3048951c
Alexandru Maftei <mafterr11@gmail.com>
2024-06-19 15:44:48 +0300

    .

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,4 +1,4 @@
-import { Roboto, Recursive} from "next/font/google";
+import { Roboto, Recursive } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -11,35 +11,34 @@ import { constructMetadata } from "@/lib/utils";
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
-  variable: "--font-roboto"
+  variable: "--font-roboto",
 });
 const recursive = Recursive({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900", "1000"],
-  variable: "--font-recursive"
+  variable: "--font-recursive",
 });
 
 export const metadata = constructMetadata();
-
 
 export default function RootLayout({ children, params: { locale } }) {
   const messages = useMessages();
   return (
     <html lang={locale}>
       <body className={`${roboto.variable} ${recursive.variable}`}>
-        <NextIntlClientProvider
-          messages={pick(
+        <NextIntlClientProvider messages={messages}>
+          {/* messages={pick(
             messages,
             "Hero",
             "About",
             "Services",
             "Work",
+            "Reviews",
             "Cta",
             "Nav",
             "Proiecte",
             "Contact",
-          )}
-        >
+          )} */}
           <Header />
           {children}
           <Analytics />
--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -5,11 +5,49 @@ import { InfiniteMovingCards } from "./ui/infinite-cards";
 import { Star } from "lucide-react";
 import HalfStar from "./ui/half-star";
 import { Dot } from "./Dot";
+import { useTranslations } from "next-intl";
 
 export function Reviews() {
+  const t = useTranslations("Reviews");
+  const testimonials = [
+    {
+      quote: t("madiny"),
+      name: "Madiny",
+      title: "https://madinytattoo.ro",
+      star: <Star strokeWidth={0.8} fill="#FFDF00" />,
+    },
+    {
+      quote: t("tng"),
+      name: "Mihai",
+      title: "https://tngag.ro",
+      star: <Star strokeWidth={0.8} fill="#FFDF00" />,
+    },
+    {
+      quote: t("monte-bianco"),
+      name: "Monte Bianco",
+      title: "https://montebianco.ro",
+      star: <HalfStar />,
+    },
+    {
+      quote: t("mbody"),
+      name: "Mihaela",
+      title: "https://mbody.vercel.app",
+      star: <Star strokeWidth={0.8} fill="#FFDF00" />,
+    },
+    {
+      quote: t("dianazu"),
+      name: "Diana",
+      title: "https://dianazu.vercel.app",
+      star: <HalfStar />,
+    },
+  ];
+
   return (
     <div className="relative mb-12 flex h-[45rem] flex-col items-center justify-center overflow-hidden rounded-md antialiased xl:mb-24">
-      <h1 className="section-title mb-16"><Dot />What Our Clients Say</h1>
+      <h1 className="section-title mb-16">
+        <Dot />
+        {t("title")}
+      </h1>
       <InfiniteMovingCards
         items={testimonials}
         direction="right"
@@ -18,41 +56,3 @@ export function Reviews() {
     </div>
   );
 }
-
-const testimonials = [  
-  {
-    quote:
-      "Am colaborat cu Alexandru pentru crearea site-ului meu și pot spune că experiența a fost fantastică. Site-ul este animat și minimalist, ceea ce îmi pune în valoare lucrările de artă într-un mod foarte profesionist.",
-    name: "Madiny",
-    title: "https://madinytattoo.ro",
-    star: <Star strokeWidth={0.8} fill="#FFDF00" />,
-  },
-  {
-    quote:
-      "Am comunicat foarte eficient pe parcursul întregului proces, iar acest lucru s-a reflectat în calitatea finală a site-ului. Nu am întâmpinat probleme tehnice, ceea ce este esențial pentru o prezență online fără griji. Mulțumesc pentru dedicare și profesionalism!",
-    name: "Mihai",
-    title: "https://tngag.ro",
-    star: <Star strokeWidth={0.8} fill="#FFDF00" />,
-  },
-  {
-    quote:
-      "Colaborarea cu Alexandru pentru crearea site-ului Montebianco.ro a fost o alegere excelentă, deoarece toate cerințele noastre au fost întâmpinate cu soluții creative și eficiente. Recomand cu căldură serviciile sale tuturor celor care își doresc un site funcțional și atractiv.",
-    name: "Monte Bianco",
-    title: "https://montebianco.ro",
-    star: <HalfStar />,
-  },
-  {
-    quote:
-      "Site-ul creat pentru serviciile mele de personal training este exact ceea ce îmi doream. Design-ul este dinamic și inspirațional, exact ca antrenamentele pe care le ofer. Trecerea de la o secțiune la alta este fluidă, iar utilizatorii pot găsi ușor informațiile de care au nevoie. Mulțumesc pentru efortul de a înțelege și de a transpune în realitate viziunea mea!",
-    name: "Mihaela",
-    title: "https://mbody.vercel.app",
-    star: <Star strokeWidth={0.8} fill="#FFDF00" />,
-  },
-  {
-    quote:
-      "Ca designer grafic, am nevoie de un site care să-mi pună în valoare lucrările, și exact asta ai realizat. Designul este curat și modern, iar portofoliul meu arată fantastic pe orice dispozitiv. ",
-    name: "Diana",
-    title: "https://dianazu.vercel.app",
-    star: <HalfStar />,
-  },
-];
