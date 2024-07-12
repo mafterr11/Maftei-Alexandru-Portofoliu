b053591153d3243d0c6a2c08977b91c47a45ea65
Alexandru Maftei <mafterr11@gmail.com>
2024-07-12 14:59:37 +0300

    .

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function constructMetadata({
-  title = "Myriad Tech - Web Development Bucuresti",
-  description = "Alege Myriad Tech pentru servicii personalizate de web development, transformând viziunea afacerii tale în realitate prin soluții digitale inovatoare.",
-  keywords = "dezvoltare web, design web personalizat, dezvoltator web freelancer, site-uri responsive, solutii e-commerce, Myriad Tech, design web, freelancer, bucuresti, romania, afacere mica, maftei alexandru, preturi site, super pret",
+  title = "Servicii Profesionale de Web Design și Development - Myriad Tech",
+  description = "Oferim servicii complete de web design și development, de la creare site-uri de prezentare la soluții de e-commerce personalizate. Contactează Myriad Tech pentru un site modern și optimizat SEO.",
+  keywords = "creare site web, creare site, firme creare site uri, web design bucuresti, servicii web design, pret site web, design web personalizat, solutii e-commerce, Myriad Tech, design web, maftei alexandru, preturi site",
   image = "/logo.png",
   icons = "/icon.svg",
 }: {
@@ -27,12 +27,12 @@ export function constructMetadata({
       title,
       description,
       images: [{ url: image }],
-      type: 'website',
+      type: "website",
     },
     twitter: {
       card: "summary_large_image",
       title,
-      site:"https://myriad-tech.ro",
+      site: "https://myriad-tech.ro",
       description,
       images: [image],
       creator: "@myriad-tech",
