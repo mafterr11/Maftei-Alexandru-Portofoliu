89177a5e902408418d81dbb3f09dd2b52c6b418d
Alexandru Maftei <mafterr11@gmail.com>
2024-08-06 11:56:59 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ro", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: "/ro/projects",
+        destination: "/ro/proiecte", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default withNextIntl(nextConfig);
