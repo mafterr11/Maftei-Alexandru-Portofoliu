7460770da485c9cb4dffb64356d6e394eeff29be
mafterr11 <mafterr11@gmail.com>
2025-02-04 22:33:18 +0200

    .

--- components/Dot.jsx
+++ components/Dot.jsx
@@ -2,7 +2,7 @@ export const Dot = () => {
   return (
     <div className="relative text-[65px] text-accent">
       &#x2022;{" "}
-      <span className=" absolute right-0 top-1 h-4 w-4 rounded-full border-2 border-black-heavy/50"></span>
+      <span className=" absolute right-0 top-[9px] h-4 w-4 rounded-full border-2 border-black-heavy/50"></span>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,100 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+      "2xl": "1500px",
+    },
+    colors: {
+      accent: "#796250",
+      body: {
+        DEFAULT: "#fbfdff",
+        light: "#EAE9E7",
+      },
+      white: "#fff",
+      black: {
+        DEFAULT: "#0d0c0b",
+        heavy: "#000000",
+      },
+      grey: {
+        light: "#ECECEC",
+      },
+      red: {
+        DEFAULT: "#ED2939",
+      },
+    },
+    extend: {
+      height: {
+        'screen-vh': 'calc(var(--vh, 1vh) * 100)', 
+      },
+      boxShadow: {
+        button: "2px 3px 3px rgba(0, 0, 0, 0.25)",
+      },
+      backgroundImage: {
+        work: "url('/project-bg-light.png')",
+        contact: "url('/illustration-dark.svg')",
+        grainy: "url('/grainy-bg.webp')",
+        softPaper: "url('/Background testimoniale.webp')",
+      },
+      fontFamily: {
+        roboto: [`var(--font-roboto)`, "sans-serif"],
+        recursive: [`var(--font-recursive)`, "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors
+  ],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
