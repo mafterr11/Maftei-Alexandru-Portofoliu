d26536701cfabcb57ea66dc9b0d05f3e744ecc44
Alexandru Maftei <mafterr11@gmail.com>
2025-02-27 15:10:13 +0200

    .

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,25 +1,25 @@
-import { Roboto, Recursive } from 'next/font/google';
-import './globals.css';
-import Header from '../../components/layout/Header';
-import Footer from '../../components/layout/Footer';
-import { Toaster } from '@/components/ui/toaster';
-import { NextIntlClientProvider } from 'next-intl';
-import { Analytics } from '@vercel/analytics/react';
-import { constructMetadata } from '@/lib/utils';
-import { Suspense } from 'react';
-import GoogleAnalytics from '@/components/google-analytics';
-import CookieBanner from '@/components/cookie-banner';
-import { getMessages } from 'next-intl/server';
+import { Roboto, Recursive } from "next/font/google";
+import "./globals.css";
+import Header from "../../components/layout/Header";
+import Footer from "../../components/layout/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import { NextIntlClientProvider } from "next-intl";
+import { Analytics } from "@vercel/analytics/react";
+import { constructMetadata } from "@/lib/utils";
+import { Suspense } from "react";
+import GoogleAnalytics from "@/components/google-analytics";
+import CookieBanner from "@/components/cookie-banner";
+import { getMessages } from "next-intl/server";
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '700', '900'],
-  variable: '--font-roboto',
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
 });
 const recursive = Recursive({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900', '1000'],
-  variable: '--font-recursive',
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900", "1000"],
+  variable: "--font-recursive",
 });
 
 export const metadata = constructMetadata();
