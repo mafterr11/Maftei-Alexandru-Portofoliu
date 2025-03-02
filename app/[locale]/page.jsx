55b7086942d85cefcf3b7a046909ad03e5b74692
mafterr11 <mafterr11@gmail.com>
2025-03-02 16:36:55 +0200

    .

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,5 +1,9 @@
+import About from "@/components/home/About";
+import Cta from "@/components/home/Cta";
 import Hero from "@/components/home/Hero";
 import { Reviews } from "@/components/home/Reviews";
+import Services from "@/components/home/Services";
+import Work from "@/components/home/Work";
 
 
 export default function Home() {
